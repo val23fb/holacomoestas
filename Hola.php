f29c31f5048478fe0889bfd55c2fd33a4ca293c9
val23fb <fatima14dcm13@gmail.com>
2025-09-24 17:01:10 -0600

    Update Hola.php

--- Hola.php
+++ Hola.php
@@ -1,15 +1,17 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Hola a todos</title>
+    <title>Hola a todos</title>
 </head>
 <body>
-	<h1>Fecha: <? = date("d.m.y")?><</h1>
-	<?php 
-	if (date ("d") % 2 ==0){
-		echo "hoy es un dia par";
-	}else{
-		echo "hoy es un dia impar";
-	}
-	?>
+    <h1>Fecha: <?= date("d.m.y") ?></h1>
+    <?php 
+    if (date("d") % 2 == 0) {
+        echo "Hoy es un día par";
+    } else {
+        echo "Hoy es un día impar";
+    }
+    ?>
 </body>
 </html>
+
